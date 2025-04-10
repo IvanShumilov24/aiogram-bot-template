9ca9a1f649f0a2a8b975cfda88ced6c739dc6d1b
ulver <ivansergshum@gmail.com>
2025-04-10 10:07:27 +0300

    add docker

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.database.models.base import Base
+from app.database.models.user import User
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
